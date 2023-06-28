4fdda7acca3c77d34b25921d75b0e432ed4458bd
bmatchen <bmatchen@gmail.com>
2023-06-27 21:50:07 -0400

    add gitignore and hello world

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// Suggested pre-commit actions:
+// * cargo build --verbose
+// * cargo clippy --all-targets -- -D warnings
+// * cargo test --verbose
+//   * from ./ (src/) or ../
+// * cargo run
+//   * from ./ (src/)
+fn main() {
+    println!("Hello, world!");
+}
