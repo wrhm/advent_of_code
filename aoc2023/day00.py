2485e2371c2e1bc48156dc78b45a180bc8842f26
bmatchen <bmatchen@gmail.com>
2023-12-02 10:16:52 -0500

    solve day 2 py

---
+++ aoc2023/day00.py
@@ -0,0 +1,5 @@
+# day00.py
+
+import util
+
+lines = util.file_as_lines('00/a.txt')
---
+++ aoc2023/day02.py
@@ -0,0 +1,46 @@
+# day02.py
+
+import util
+
+lines = util.file_as_lines('02/a.txt')
+
+example = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''.split('\n')
+
+
+def is_possible(line):
+    [lhs, rhs] = line.split(':')
+    gn = lhs.split(' ')[1]
+    grabs = rhs.split(';')
+    d = {'red': 0, 'green': 0, 'blue': 0}
+    for pg in grabs:
+        vks = [x.split() for x in pg.split(',')]
+        for [v, k] in vks:
+            d[k] = max(d[k], int(v))
+    r, g, b = d['red'], d['green'], d['blue']
+    possible = r <= 12 and g <= 13 and b <= 14
+    return int(gn) if possible else 0
+
+
+# print(sum([is_possible(line) for line in example]))
+print(sum([is_possible(line) for line in lines]))
+
+
+def find_power(line):
+    [lhs, rhs] = line.split(':')
+    gn = lhs.split(' ')[1]
+    grabs = rhs.split(';')
+    d = {'red': 0, 'green': 0, 'blue': 0}
+    for pg in grabs:
+        vks = [x.split() for x in pg.split(',')]
+        for [v, k] in vks:
+            d[k] = max(d[k], int(v))
+    r, g, b = d['red'], d['green'], d['blue']
+    return r*g*b
+
+
+# print(sum([find_power(line) for line in example]))
+print(sum([find_power(line) for line in lines]))
