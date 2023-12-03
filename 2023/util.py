a5251f5db328e5e53b08c4712871fa4bd699a7f1
bmatchen <bmatchen@gmail.com>
2023-12-03 16:13:22 -0500

    add shared utils

--- 2023/util.py
+++ 2023/util.py
@@ -1,3 +1,11 @@
+import sys
+import os
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(parent_dir)
+sys.path.append(parent_dir+'/..')
+import common.shared_utils as su
+
+
 def file_as_lines(filename):
     lines = []
     with open(filename) as fd:
