e51424e6007f71fa771fe8499159ab79b1956d25
bmatchen <bmatchen@gmail.com>
2023-12-03 10:50:47 -0500

    more exploring with ocaml

--- aoc2023/aoc.ml
+++ aoc2023/aoc.ml
@@ -13,28 +13,28 @@ In this example, the calibration values of these four lines are 12, 38, 15, and 
 
 Consider your entire calibration document. What is the sum of all of the calibration values?   
 *)
-
 open Base
 open Stdio
+(* open Fmt *)
 (* open List *)
 (* open Char *)
 
 (* let example = "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet"
+treb7uchet" *)
 
-let lines = String.split_lines example *)
+(* let lines = String.split_lines example *)
 
 let is_digit x =
-  Char.to_int '0' <= Char.to_int x && Char.to_int x <= Char.to_int '9'
+  Base.Char.to_int '0' <= Base.Char.to_int x && Base.Char.to_int x <= Base.Char.to_int '9'
 
-let find_digits s =
+let find_digits ch_list =
   let rec h st =
     match st with
     | [] -> []
     | x :: xs -> (if is_digit x then (Char.to_int x)-(Char.to_int '0') :: h xs else h xs)
-  in h s
+  in h ch_list
 
 (* let () = printf "%b\n" @@ is_digit '3'; *)
 (* let () = printf "%b\n" @@ (find_digits (Base.String.to_list "123")); *)
@@ -47,15 +47,64 @@ let first_elem = match List.hd a with
  | _ -> 0;;
 let () = printf "%d\n" @@ first_elem
 
+let print_num n = printf "%d\n" @@ n
+
+let hd_or_default d xs =
+  match List.hd xs with
+ | Some x -> x
+ | _ -> d;; 
+
+let last_or_default d xs =  
+  hd_or_default d (List.rev xs);; 
+
+(* 10*first + last *)
+let fl_num xs =
+  let h = hd_or_default 0 xs in
+  let last = last_or_default 0 xs
+in h*10+last
+
+let () = print_num ( fl_num [1;2])
+
+
+(* let formatter_int_list = Fmt.list Fmt.int
+
+let () = Format.asprintf "%a" formatter_int_list [1;2] *)
+
+(* https://gist.github.com/flux77/ab4c20ee28fc3742df29c4f790e6e65f *)
+let print_list to_string l =
+  let rec loop rem acc = 
+    match rem with
+    | [] -> acc
+    | [x] -> acc ^ (to_string x)
+    | (x::xs) -> loop xs (acc ^ (to_string x) ^ "; ") in
+  print_string "[";
+  print_string (loop l "");
+  print_endline "]"
+
+let _print_int_list = print_list Int.to_string
+
+(* let lst = [1;2]
+let () = print_int_list lst
+
+let () = print_int_list a *)
+
 (* let rec digits s =
   match  *)
 
-(* let solve filename =
-  let content = In_channel.read_all filename in
-  (* let lines = String.split_lines content in *)
-  printf "%s\n" @@ content;
-  printf "%s\n" @@ filename;;
+(* let explode_string s = List.init (String.length s) (String.get s);; *)
 
-let () = solve "01/a.txt" *)
+(* let explode_string *)
+
+let solve filename =
+  let content = In_channel.read_all filename in
+  let lines = String.split_lines content in
+  (* printf "%s\n" @@ content; *)
+  (* printf "%s\n" @@ filename;; *)
+  let fls = List.map ~f:(fun x -> fl_num (find_digits ( x))) lines in
+  (* let fls = List.map (fun x -> x) lines in *)
+  _print_int_list fls;;
+
+(* let () = solve "01/a.txt" *)
+(* let _x = List.map ~f:(fun x -> x) [1]  *)
 
 (* let () = printf "%s\n" @@ example; *)
