f7f4a8918a8a5902f52812d18849904a8fb0587d
bmatchen <bmatchen@gmail.com>
2023-12-16 13:26:55 -0500

    2023 day 13 part 2

--- 2023/day13.py
+++ 2023/day13.py
@@ -57,21 +57,18 @@ def disp(g):
         print(r)
 
 
+def dispj(g):
+    for r in g:
+        print(''.join(r))
+
+
 def vert_line_of_sym(g):
     w, h = len(g[0]), len(g)
-    print('\n')
-    disp(g)
-    print('\n')
+    # print('\n')
+    # disp(g)
+    # print('\n')
     d = dict()
     for r in g:
-        # for i in range(w):
-        #     for j in range(i+1, w):
-        #         if row_sym(r, i, j):
-        #             # print(i, j, r)
-        #             if (i, j) in d:
-        #                 d[(i, j)] += 1
-        #             else:
-        #                 d[(i, j)] = 1
         syms = []
         for i in range(w-1):
             if row_sym(r, i, i+1):
@@ -81,16 +78,38 @@ def vert_line_of_sym(g):
                     d[(i, i+1)] += 1
                 else:
                     d[(i, i+1)] = 1
-        print(r, syms)
-    print(d)
-    print('want v=', h)
+        # print(r, syms)
+    # print(d)
+    # print('want v=', h)
     ret = None
     for k in d:
         (l, r) = k
         if d[k] == h:
-            print(k, d[k])
+            # print(k, d[k])
             if ret is None or l > ret:
                 ret = l
+    # print(d)
+    # print('h', h, 'mv', max(d.values()), 'ret', ret)
+    return ret
+
+
+def vert_lines_of_sym(g):
+    w, h = len(g[0]), len(g)
+    d = dict()
+    for r in g:
+        syms = []
+        for i in range(w-1):
+            if row_sym(r, i, i+1):
+                syms.append((i, i+1))
+                if (i, i+1) in d:
+                    d[(i, i+1)] += 1
+                else:
+                    d[(i, i+1)] = 1
+    ret = []
+    for k in d:
+        (l, r) = k
+        if d[k] == h:
+            ret.append(l)
     return ret
 
 
@@ -103,28 +122,93 @@ def solve_pt1(data):
     vsum = 0
     hsum = 0
     for g in parse(data):
+        # print(g)
         gt = transpose(g)
+
         vs_or = vert_line_of_sym(g)
+        print('=')
         hs_or = vert_line_of_sym(gt)
         print(vs_or, hs_or)
         if vs_or is not None:
             vsum += 1+vs_or
         if hs_or is not None:
             hsum += 1+hs_or
-        # v = vert_line_of_sym(g)+1
-        # h = vert_line_of_sym(gt)+1
-        # print(v, h, 100*h+v)
-
-        # break
     return vsum+100*hsum
 
 
+def opp(c):
+    if c == '.':
+        return '#'
+    return '.'
+
+
 def solve_pt2(data):
-    return ''
+    vsum = 0
+    hsum = 0
+    for g in parse(data):
+        w, h = len(g[0]), len(g)
+        g = [[x for x in y] for y in g]
+        # print(g)
+        gt = transpose(g)
+        print('====')
+        # dispj(g)
+        vs_or = vert_lines_of_sym(g)
+        hs_or = vert_lines_of_sym(gt)
+        print('orig', vs_or, hs_or)
+        found = False
+        # for r in range(h):
+        #     for c in range(w):
+        #         g[r][c] = opp(g[r][c])
+        #         gt = transpose(g)
+        #         vs_or2 = vert_line_of_sym(g)
+        #         hs_or2 = vert_line_of_sym(gt)
+        #         g[r][c] = opp(g[r][c])
+        #         # print(r, c, vs_or2, hs_or2)
+        #         if (vs_or2, hs_or2) != (None, None) and (vs_or2, hs_or2) != (vs_or, hs_or):
+        #             print('new', vs_or2, hs_or2)
+        #             g[r][c] = opp(g[r][c])
+        #             # dispj(g)
+        #             g[r][c] = opp(g[r][c])
+        #             if vs_or2 is not None and vs_or2 != vs_or:
+        #                 vsum += 1+vs_or2
+        #             if hs_or2 is not None and hs_or2 != hs_or:
+        #                 hsum += 1+hs_or2
+        #             found = True
+        #         if found:
+        #             break
+        #     if found:
+        #         break
+        for r in range(h):
+            for c in range(w):
+                g[r][c] = opp(g[r][c])
+                gt = transpose(g)
+                vs_or2 = vert_lines_of_sym(g)
+                hs_or2 = vert_lines_of_sym(gt)
+                g[r][c] = opp(g[r][c])
+                # print(r, c, vs_or, hs_or, vs_or2, hs_or2)
+                if (vs_or2, hs_or2) != ([], []):
+                    vsd = set(vs_or2)-set(vs_or)
+                    hsd = set(hs_or2)-set(hs_or)
+                    if len(vsd) > 0:
+                        newv = vsd.pop()
+                        vsum += 1+newv
+                        found = True
+                    if len(hsd) > 0:
+                        newh = hsd.pop()
+                        hsum += 1+newh
+                        found = True
+                if found:
+                    break
+            if found:
+                break
+        assert(found)
+        if not found:
+            print('no solution')
+    return vsum+100*hsum
 
 
 if __name__ == '__main__':
-    print('ex1', solve_pt1(example))
-    print('part1', solve_pt1(lines))
-    # print('ex2', solve_pt2(example))
-    # print('part2', solve_pt2(lines))
+    # print('ex1', solve_pt1(example))
+    # print('part1', solve_pt1(lines))
+    print('ex2', solve_pt2(example))
+    print('part2', solve_pt2(lines))
