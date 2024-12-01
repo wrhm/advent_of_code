91396f0e3e4a212b89b3ee5eae1b05982c741236
bmatchen <bmatchen@gmail.com>
2024-12-01 11:10:00 -0500

    2024 day 01 part 2

--- 2024/day01/day01.go
+++ 2024/day01/day01.go
@@ -16,56 +16,72 @@ var example = `3   4
 3   9
 3   3`
 
-func sumDiffs(contents string) {
-	fmt.Println(contents)
+// func numLists() ([]int, []int) {
+
+// }
+
+func numLists(contents string) ([]int, []int) {
 	var list0 []int
 	var list1 []int
 	lines_ex := strings.Split(contents, "\n")
 	for _, line := range lines_ex {
-		// vals := strings.Split(line, " ")
 		vals := strings.Fields((line))
-		// fmt.Println(line, vals)
-		// for _, v := range vals {
-		// 	fmt.Println(v)
-		// }
 		vi0, _ := strconv.Atoi(vals[0])
 		vi1, _ := strconv.Atoi(vals[1])
 		list0 = append(list0, vi0)
 		list1 = append(list1, vi1)
 	}
-	fmt.Println(list0)
-	fmt.Println(list1)
+	return list0, list1
+}
+
+func sumDiffs(contents string) {
+	list0, list1 := numLists(contents)
 	sort.Ints(list0)
 	sort.Ints(list1)
-	fmt.Println(list0)
-	fmt.Println(list1)
 	var total_diffs = 0
 	for i, v := range list0 {
 		total_diffs += int(math.Abs(float64(v - list1[i])))
 	}
-	fmt.Printf("total: %d\n", total_diffs)
+	fmt.Printf("part 1 total: %d\n", total_diffs)
+}
+
+func counts(nums []int) map[int]int {
+	ret := make(map[int]int)
+	for _, v := range nums {
+		val, ok := ret[v]
+		if ok {
+			ret[v] = val + 1
+		} else {
+			ret[v] = 1
+		}
+	}
+	return ret
+}
+
+func partTwo(contents string) {
+	list0, list1 := numLists(contents)
+	right := counts(list1)
+	// fmt.Println(right)
+	ret := 0
+	for _, v := range list0 {
+		val, ok := right[v]
+		if ok {
+			// fmt.Printf("%d occurs %d times in list1\n", v, val)
+			ret += v * val
+		} else {
+			// fmt.Printf("%d is missing from list1\n", v)
+		}
+	}
+	fmt.Printf("part 2 total: %d\n", ret)
 }
 
 func main() {
-	// fmt.Printf("%d\n", 123)
-	// fmt.Printf("%s\n", example)
-	// dat, _ := os.ReadFile("day01.txt")
-	// fmt.Println(string(dat))
-
+	fmt.Println("Example:")
 	sumDiffs(example)
-
-	// file, err := os.Open("day01.txt")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// defer file.Close()
-	// scanner := bufio.NewScanner(file)
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// 	fmt.Println(line)
-	// }
-
+	partTwo(example)
 	data, _ := ioutil.ReadFile("day01.txt")
 	content := string(data)
+	fmt.Println("\nFrom file:")
 	sumDiffs(string(content))
+	partTwo(string(content))
 }
