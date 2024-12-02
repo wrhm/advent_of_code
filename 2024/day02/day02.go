4036e1bce85890f3ed0be5b6ef74029c1dc79406
bmatchen <bmatchen@gmail.com>
2024-12-02 11:44:51 -0500

    day 02 part 2

--- 2024/day02/day02.go
+++ 2024/day02/day02.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+
+	// "slices"
 	"strconv"
 	"strings"
 )
@@ -78,25 +80,77 @@ func hasBoundedDiffs(vals []int) bool {
 	return true
 }
 
+func isValid(vals []int) bool {
+	return (isIncreasing(vals) || isDecreasing(vals)) && hasBoundedDiffs(vals)
+}
+
 func partOne(contents string) {
 	var lists = numLists2d(contents)
-	// fmt.Println(lists)
 	var ret = 0
 	for _, t := range lists {
-		if (isIncreasing(t) || isDecreasing(t)) && hasBoundedDiffs(t) {
+		if isValid(t) {
 			ret++
 		}
 	}
 	fmt.Printf("part 1 total: %d\n", ret)
 }
 
+// returns all cases of removing a single element from vals.
+func removals(vals []int) [][]int {
+	// fmt.Println(vals)
+
+	var ret [][]int
+	for i := range vals {
+		var row []int
+		// if i == 0 || i == len(vals) {
+		// 	continue
+		// }
+		// fmt.Println(append(vals[:i+1], vals[i+2:]...))
+		// fmt.Println(i, vals[:i], vals[i:])
+		left := vals[:i]
+		right := vals[i+1:]
+		// joined := append(left, right...)
+		// fmt.Println(i, vals[:i], vals[i], vals[i+1:]) //, joined)
+		for _, v := range left {
+			row = append(row, v)
+		}
+		for _, v := range right {
+			row = append(row, v)
+		}
+		// fmt.Println(row)
+		ret = append(ret, row)
+	}
+	return ret
+
+}
+
+func partTwo(contents string) {
+	var lists = numLists2d(contents)
+	var ret = 0
+	for _, t := range lists {
+		if isValid(t) {
+			ret++
+		} else {
+			rems := removals(t)
+			for _, v := range rems {
+				if isValid(v) {
+					ret++
+					break
+				}
+			}
+		}
+		// break
+	}
+	fmt.Printf("part 2 total: %d\n", ret)
+}
+
 func main() {
 	fmt.Println("Example:")
 	partOne(example)
-	// partTwo(example)
+	partTwo(example)
 	data, _ := ioutil.ReadFile("day02.txt")
 	content := string(data)
 	fmt.Println("\nFrom file:")
 	partOne(string(content))
-	// partTwo(string(content))
+	partTwo(string(content))
 }
