802ac5e0f799146e6d8084813f65a38fae2b843d
bmatchen <bmatchen@gmail.com>
2024-12-08 13:03:53 -0500

    clean up day 08

--- 2024/day08.go
+++ 2024/day08.go
@@ -43,6 +43,21 @@ func antinode(r1 int, c1 int, r2 int, c2 int) (int, int) {
 	return r2 + (r2 - r1), c2 + (c2 - c1)
 }
 
+// from byte to slice of points
+func gridPointsWithChar(bs *([][]byte)) map[byte][]*GridPoint {
+	h := len(*bs)
+	w := len((*bs)[0])
+	points_per_ch := make(map[byte][]*GridPoint)
+	for r := 0; r < h; r++ {
+		for c := 0; c < w; c++ {
+			ch := (*bs)[r][c]
+			gp := GP(r, c)
+			points_per_ch[ch] = append(points_per_ch[ch], gp)
+		}
+	}
+	return points_per_ch
+}
+
 func day08partOne(contents string) {
 	start := time.Now()
 	fmt.Printf("contents has size %d\n", len(contents))
@@ -50,47 +65,25 @@ func day08partOne(contents string) {
 	bs := strListAs2dBytes(lines)
 	h := len(bs)
 	w := len(bs[0])
-	// from byte to slice of points
-	points_per_ch := make(map[byte][]*GridPoint)
-	for r := 0; r < h; r++ {
-		for c := 0; c < w; c++ {
-			ch := bs[r][c]
-			gp := GP(r, c)
-			points_per_ch[ch] = append(points_per_ch[ch], gp)
-		}
-	}
+	points_per_ch := gridPointsWithChar(&bs)
 	antinodes := make2dPointSet()
 	for k := range points_per_ch {
 		if k == '.' {
 			continue
 		}
-		// fmt.Printf("\n%c: ", k)
-		// for _, gp := range points_per_ch[k] {
-		// 	fmt.Printf("(%d,%d) ", gp.r, gp.c)
-		// }
 		for _, pi := range points_per_ch[k] {
 			for _, pj := range points_per_ch[k] {
 				if pi == pj {
 					continue
 				}
 				anr, anc := antinode(pi.r, pi.c, pj.r, pj.c)
-				// fmt.Printf("\nantinode of (%d,%d),(%d,%d) is (%d,%d)", pi.r, pi.c, pj.r, pj.c, anr, anc)
 				if inBounds(anr, 0, h-1) && inBounds(anc, 0, w-1) {
-					// fmt.Printf("\ninserting #%d: (%d,%d)", len(antinodes)+1, anr, anc)
 					insertInto2dPointSet(&antinodes, anr, anc)
-
 				}
 			}
 		}
 	}
 	fmt.Println()
-	// fmt.Println(antinodes)
-	// var aks []string
-	// for k := range antinodes {
-	// 	aks = append(aks, k)
-	// }
-	// sort.Strings(aks)
-	// fmt.Println(aks)
 	var ret = len(antinodes)
 	LogPartOneResult(ret, start)
 }
@@ -98,7 +91,8 @@ func day08partOne(contents string) {
 func collinear(r1 int, c1 int, r2 int, c2 int, r3 int, c3 int) bool {
 	dr := r2 - r1
 	dc := c2 - c1
-	// return (r3-r1)%dr == 0 && (c3-c1)%dc == 0
+	// point 3's difference from point 1 needs to be a whole multiple
+	// of the difference of points 1 and 2.
 	if (r3-r1)%dr != 0 || (c3-c1)%dc != 0 {
 		return false
 	}
@@ -112,41 +106,21 @@ func day08partTwo(contents string) {
 	bs := strListAs2dBytes(lines)
 	h := len(bs)
 	w := len(bs[0])
-	// from byte to slice of points
-	points_per_ch := make(map[byte][]*GridPoint)
-	for r := 0; r < h; r++ {
-		for c := 0; c < w; c++ {
-			ch := bs[r][c]
-			gp := GP(r, c)
-			points_per_ch[ch] = append(points_per_ch[ch], gp)
-		}
-	}
+	points_per_ch := gridPointsWithChar(&bs)
 	antinodes := make2dPointSet()
 	for k := range points_per_ch {
 		if k == '.' {
 			continue
 		}
-		// fmt.Printf("\n%c: ", k)
-		// for _, gp := range points_per_ch[k] {
-		// 	fmt.Printf("(%d,%d) ", gp.r, gp.c)
-		// }
 		for _, pi := range points_per_ch[k] {
 			for _, pj := range points_per_ch[k] {
 				if pi == pj {
 					continue
 				}
-				// anr, anc := antinode(pi.r, pi.c, pj.r, pj.c)
-				// // fmt.Printf("\nantinode of (%d,%d),(%d,%d) is (%d,%d)", pi.r, pi.c, pj.r, pj.c, anr, anc)
-				// if inBounds(anr, 0, h-1) && inBounds(anc, 0, w-1) {
-				// 	// fmt.Printf("\ninserting #%d: (%d,%d)", len(antinodes)+1, anr, anc)
-				// 	insertInto2dPointSet(&antinodes, anr, anc)
-				// }
 				for r := 0; r < h; r++ {
 					for c := 0; c < w; c++ {
 						// include if equally spaced on a line with pi and pj
-
 						if collinear(pi.r, pi.c, pj.r, pj.c, r, c) {
-							fmt.Printf("\nline (%d,%d),(%d,%d) includes (%d,%d)", pi.r, pi.c, pj.r, pj.c, r, c)
 							if inBounds(r, 0, h-1) && inBounds(c, 0, w-1) {
 								insertInto2dPointSet(&antinodes, r, c)
 							}
@@ -157,13 +131,6 @@ func day08partTwo(contents string) {
 		}
 	}
 	fmt.Println()
-	// fmt.Println(antinodes)
-	// var aks []string
-	// for k := range antinodes {
-	// 	aks = append(aks, k)
-	// }
-	// sort.Strings(aks)
-	// fmt.Println(aks)
 	var ret = len(antinodes)
 	LogPartTwoResult(ret, start)
 }
