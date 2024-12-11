a3c4255cc465ac038e8129853dfe941b87cc6d96
bmatchen <bmatchen@gmail.com>
2024-12-10 20:09:18 -0500

    clean up day 09 part 1

--- 2024/day09.go
+++ 2024/day09.go
@@ -12,14 +12,6 @@ const day09example = `2333133121414131402`
 
 // const day09example = `12345`
 
-func nRepsOf(n int, s string) string {
-	var ret string
-	for i := 0; i < n; i++ {
-		ret = ret + s
-	}
-	return ret
-}
-
 func digitRuneAsInt(r rune) int {
 	v, _ := strconv.Atoi(string(r))
 	return v
@@ -28,35 +20,6 @@ func digitRuneAsInt(r rune) int {
 func digitByteAsInt(b byte) int {
 	v, _ := strconv.Atoi(string(b))
 	return v
-}
-
-func bytesToInts(bs []byte) []int {
-	var ret []int
-	for _, v := range bs {
-		ret = append(ret, digitByteAsInt(v))
-	}
-	return ret
-}
-
-func diskMapToBlocks(dm string) string {
-	ret := ""
-	filenum := 0
-	for i, r := range dm {
-		// v, _ := strconv.Atoi(string(c))
-		v := digitRuneAsInt(r)
-		if i%2 == 0 {
-			part := nRepsOf(v, strconv.Itoa(filenum))
-			filenum++
-			// filenum = (filenum + 1) % 10
-			// fmt.Println("file", v, part)
-			ret += part
-		} else {
-			part := nRepsOf(v, ".")
-			// fmt.Println("space", v, part)
-			ret += part
-		}
-	}
-	return ret
 }
 
 func diskMapToIntBlocks(dm string) []int {
@@ -78,38 +41,6 @@ func diskMapToIntBlocks(dm string) []int {
 	return ret
 }
 
-// 5602261504 was too small
-// 89859464970 was too small
-
-func indFirst(bs []byte, b byte) int {
-	for i := 0; i < len(bs); i++ {
-		if bs[i] == b {
-			return i
-		}
-	}
-	return -1
-}
-
-func indLast(bs []byte, b byte) int {
-	ret := -1
-	for i := 0; i < len(bs); i++ {
-		if bs[i] == b {
-			ret = i
-		}
-	}
-	return ret
-}
-
-func indLastNon(bs []byte, b byte) int {
-	ret := -1
-	for i := 0; i < len(bs); i++ {
-		if bs[i] != b {
-			ret = i
-		}
-	}
-	return ret
-}
-
 func indFirstInt(vs []int, v int) int {
 	for i := 0; i < len(vs); i++ {
 		if vs[i] == v {
@@ -118,16 +49,6 @@ func indFirstInt(vs []int, v int) int {
 	}
 	return -1
 }
-
-// func indLastInt(vs []int, v int) int {
-// 	ret := -1
-// 	for i := 0; i < len(vs); i++ {
-// 		if vs[i] == v {
-// 			ret = i
-// 		}
-// 	}
-// 	return ret
-// }
 
 func indLastNonInt(vs []int, v int) int {
 	ret := -1
@@ -139,25 +60,6 @@ func indLastNonInt(vs []int, v int) int {
 	return ret
 }
 
-//	func moveLastBlock(s string) bool {
-//		bs := []byte(s)
-//
-// returns: modified
-func moveLastBlock(bs *([]byte)) bool {
-	// fmt.Println("first .", indFirst(*bs, '.'))
-	// fmt.Println("last non-.", indLastNon(*bs, '.'))
-	// if indLastNon(*bs, '.')+1 == indFirst(*bs, '.')
-	indln := indLastNon(*bs, '.')
-	indf := indFirst(*bs, '.')
-	if indln < indf {
-		// fmt.Println("nothing to change")
-		return false
-	}
-	(*bs)[indf] = (*bs)[indln]
-	(*bs)[indln] = '.'
-	return true
-}
-
 func moveLastIntBlock(vs *([]int)) bool {
 	indln := indLastNonInt(*vs, -1)
 	indf := indFirstInt(*vs, -1)
@@ -167,15 +69,6 @@ func moveLastIntBlock(vs *([]int)) bool {
 	(*vs)[indf] = (*vs)[indln]
 	(*vs)[indln] = -1
 	return true
-}
-
-// func filesystemChecksum(bs []byte) int {
-func filesystemChecksum(s string) int {
-	ret := 0
-	for i, r := range s {
-		ret += i * digitRuneAsInt(r)
-	}
-	return ret
 }
 
 func filesystemIntsChecksum(vs *([]int)) int {
@@ -193,20 +86,6 @@ func day09partOne(contents string) {
 	lines := strings.Split(contents, "\n")
 	fmt.Printf("lines has size %d\n", len(lines))
 	fmt.Println(lines[0])
-	// dmtb := diskMapToBlocks(lines[0])
-	// fmt.Println(dmtb)
-	// bs := []byte(dmtb)
-	// fmt.Println(string(bs))
-	// for i := 0; i < 1e6; i++ {
-	// 	if !moveLastBlock(&bs) {
-	// 		break
-	// 	}
-	// 	fmt.Println("moving block", i, "among", len(bs), "bytes")
-	// 	// fmt.Println(string(bs))
-	// }
-	// fmt.Println("final", string(bs))
-	// var ret = filesystemChecksum(string(bs))
-
 	dmtb := diskMapToIntBlocks(lines[0])
 	fmt.Println(dmtb)
 	for i := 0; i < 1e6; i++ {
