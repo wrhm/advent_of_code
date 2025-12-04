e46d25d389a1e1175fdb9716d4664e7dfd086480
bmatchen <bmatchen@gmail.com>
2025-12-03 21:56:28 -0500

    Move example inputs into files

--- 2025/aoc2025/test/test_main.ml
+++ 2025/aoc2025/test/test_main.ml
@@ -2,18 +2,10 @@ open Aoc2025.Days
 
 let () = assert (tuple_val ("",1)==1);
 
-let example_input = {ex|
-L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82
-|ex} in
-let ex_lines = Str.split (Str.regexp "\n") example_input in
-assert ((List.length ex_lines) == 10);
-assert (d01p1 ex_lines == 3);
+let ex_lines01 = 
+  (let all_lines = Str.split (Str.regexp "\n") (read_file "../../../test/example_inputs/example01.txt") in
+  List.filter (fun s -> String.length s > 0) all_lines) in
+(* Printf.eprintf "%d\n" (List.length ex_lines01);
+Printf.eprintf "%s\n" (List.nth ex_lines01 0); *)
+assert ((List.length ex_lines01) == 10);
+assert (d01p1 ex_lines01 == 3);
