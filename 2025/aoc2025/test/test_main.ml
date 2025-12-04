3c6c26dba2b1e2bd0827d8c3db890f8770404337
bmatchen <bmatchen@gmail.com>
2025-12-03 21:17:47 -0500

    Add early tests

--- 2025/aoc2025/test/test_main.ml
+++ 2025/aoc2025/test/test_main.ml
@@ -1,3 +1,21 @@
 open Aoc2025.Days
 
-let () = assert (tuple_val ("",1)==1)
+let () = assert (tuple_val ("",1)==1);
+
+let example_input = {ex|
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+|ex} in
+let lines = Str.split (Str.regexp "\n") example_input in
+(* Printf.eprintf "%x" lines; *)
+(* List.iter print_endline lines;
+Printf.eprintf "%d" (List.length lines); *)
+assert ((List.length lines) == 10);
