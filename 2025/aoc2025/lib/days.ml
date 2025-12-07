8853995d91651c50e6b04f82a5bd955587077b8c
bmatchen <bmatchen@gmail.com>
2025-12-06 20:34:04 -0500

    solve day 02 part 2 example

--- 2025/aoc2025/lib/days.ml
+++ 2025/aoc2025/lib/days.ml
@@ -87,8 +87,39 @@ let repeats_in_range a b =
   List.filter (fun x -> (int_of_string a) <= x && x <= (int_of_string b))
   @@ List.map (fun y -> concat_nums y y) @@ range min_y max_y
 
+let list_sum = List.fold_left (+) 0
+
+(* need a fn that repeats a str n times. then need to find all strs up to 10
+digits
+that are 1-or-more repeats of a digit string. *)
+
+let rec repeat_str n s =
+  match n with
+  | 0 -> ""
+  | 1 -> s
+  | _ -> s ^ repeat_str (n-1) s
+
+let cartesian_product list_a list_b =
+  List.map (fun a -> List.map (fun b -> (a,b)) list_b) list_a |> List.flatten
+let rep_strs = List.filter (fun s -> String.length s <= 10) @@ List.map (fun (a,b) -> repeat_str a (string_of_int b)) @@ cartesian_product (range 2 10) (range 1 99999)
+
+let rec remove_adjacent_repeats list =
+  match list with
+  | [] -> []
+  | [x] -> [x]
+  | (x::y::xs) -> if x=y then remove_adjacent_repeats (y::xs) else x::(remove_adjacent_repeats (y::xs))
+
+let rep_strs_deduped = remove_adjacent_repeats @@ List.sort compare rep_strs
+
 let d02p1 lines =
   let pairs02 = List.map (String.split_on_char '-') (String.split_on_char ',' @@ List.nth lines 0) in
   let nums2d02 = List.map (fun t -> repeats_in_range (List.nth t 0) (List.nth t 1)) pairs02 in
-  let list_sum = List.fold_left (+) 0 in
   list_sum @@ List.map list_sum nums2d02
+
+let d02p2 lines =
+  let pairs02 = List.map (String.split_on_char '-') (String.split_on_char ',' @@ List.nth lines 0) in
+  let rep_ints = List.map int_of_string rep_strs_deduped in
+  let nums_in_range a b = List.filter (fun x -> a <= x && x <= b) rep_ints in
+  let nums_to_sum = List.map (fun t -> nums_in_range (int_of_string @@ List.nth t 0) (int_of_string @@ List.nth t 1)) pairs02 in
+  print_list_of_lists print_int " " nums_to_sum;
+  list_sum @@ List.map list_sum nums_to_sum
--- 2025/aoc2025/test/test_main.ml
+++ 2025/aoc2025/test/test_main.ml
@@ -23,6 +23,12 @@ assert ([1188511885]=repeats_in_range "1188511880" "1188511890");
 assert ([1010]=repeats_in_range "998" "1012");
 assert ([11]=repeats_in_range "2" "18");
 
+assert ("123123"=repeat_str 2 "123");
+assert ([(1,4);(1,5);(2,4);(2,5);(3,4);(3,5);]=cartesian_product [1;2;3] [4;5]);
+
+print_string "\n";
+(* print_string_list repstrs; *)
+
 let ex_lines02 = nonempty_lines_from_file "../../../test/example_inputs/example02.txt" in
 (* let pairs02 = List.map (String.split_on_char '-') (String.split_on_char ',' @@
 List.nth ex_lines02 0) in
@@ -33,3 +39,11 @@ print_list_of_lists print_int "/" @@ nums2d02;
 print_int @@ list_sum @@ List.map list_sum nums2d02; *)
 
 assert (d02p1 ex_lines02 = 1227775554);
+
+assert ([1;2;3]=remove_adjacent_repeats [1;1;2;3;3;3]);
+
+(* print_list_of_lists print_int " " (d02p2 ex_lines02); *)
+print_int (d02p2 ex_lines02);
+print_string "\n";
+print_int @@ List.length rep_strs;
+assert (d02p2 ex_lines02 = 4174379265);
