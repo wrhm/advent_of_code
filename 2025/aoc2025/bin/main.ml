77fbc97aaff8cfdc76d79f937e7d90d50ca3c15b
bmatchen <bmatchen@gmail.com>
2025-12-07 10:41:47 -0500

    refactor main.ml

--- 2025/aoc2025/bin/main.ml
+++ 2025/aoc2025/bin/main.ml
@@ -7,10 +7,12 @@ To continuously build/run:
 * dune exec aoc2025 -w
 *)
 
+open Aoc2025.Days
+
 let () = 
-  Printf.eprintf "\nAnswer 1p1: %d\n" (Aoc2025.Days.d01p1 Aoc2025.Days.lines01);
-  Printf.eprintf "\nAnswer 1p2: %d\n" (Aoc2025.Days.d01p2 Aoc2025.Days.lines01);
-  Printf.eprintf "\nAnswer 2p1: %d\n" (Aoc2025.Days.d02p1 Aoc2025.Days.lines02);
-  Printf.eprintf "\nAnswer 2p2: %d\n" (Aoc2025.Days.d02p2 Aoc2025.Days.lines02);
-  Printf.eprintf "\nAnswer 3p1: %d\n" (Aoc2025.Days.d03p1 Aoc2025.Days.lines03);
-  (* Printf.eprintf "\nAnswer 3p2: %d\n" (Aoc2025.Days.d03p2 Aoc2025.Days.lines03); *)
+  Printf.eprintf "\nAnswer 1p1: %d" @@ d01p1 lines01;
+  Printf.eprintf "\nAnswer 1p2: %d" @@ d01p2 lines01;
+  Printf.eprintf "\nAnswer 2p1: %d" @@ d02p1 lines02;
+  Printf.eprintf "\nAnswer 2p2: %d" @@ d02p2 lines02;
+  Printf.eprintf "\nAnswer 3p1: %d\n" @@ d03p1 lines03;
+  (* Printf.eprintf "\nAnswer 3p2: %d" @@ d03p2 lines03; *)
